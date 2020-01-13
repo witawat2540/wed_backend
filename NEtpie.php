cc3d8e276d6f5150644d1bbda4b1be6dbd4b3364
witawatd <witawatd@gmail.com>
2020-01-13 15:20:00 +0700

    netpie_cout_and_access

--- NEtpie.php
+++ NEtpie.php
@@ -1,26 +1,24 @@
+<?php
 
-<?php $data =$_GET['status'];?>
+    $objCon = mysqli_connect("localhost","iot","!Qazxsw2","iot");
+    $sqldoor = "SELECT * FROM Usagehistory WHERE id = ( SELECT MAX(id) FROM Usagehistory)";
+	$objQuerydoor = mysqli_query($objCon,$sqldoor);
+    $door = mysqli_fetch_array($objQuerydoor,MYSQLI_ASSOC);
+    $last= "Last user: ".$door['username']."";
+?>
 <script  type="text/javascript">
     var APPID= "WorkShopLED"; //enter your appid
     var KEY = "tjVAVjlbJHII6nq"; //enter your key
     var SECRET = "MgBnXJHN74Lm6ISZ3m3oogCSO"; //enter your secret
-    var Topic = "/door"; //choose any topic name
+    var Topic = "/last"; //choose any topic name
 
-  
-    function PressButtonOff(){
+    function Lastuser(){
         var url = 'https://api.netpie.io/topic/'+APPID+Topic+'?retain&auth=' +KEY+':'+SECRET;
         var xmlHttp = new XMLHttpRequest();
         xmlHttp.open('PUT',url,true);
-        xmlHttp.send(<?php echo "'$data'";?>);
+        xmlHttp.send(<?php echo "'$last'";?>);
         //window.alert(url); //for debugging purpose
     } 
-    function PressButtonOn(){
-        var url = 'https://api.netpie.io/topic/'+APPID+Topic+'?retain&auth=' +KEY+':'+SECRET;
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open('PUT',url,true);
-        xmlHttp.send('Lock');
-        //window.alert(url); //for debugging purpose
-    } 
-    PressButtonOff();
+    Lastuser();
     
 </script>
